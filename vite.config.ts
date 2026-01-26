4cb95f848fd41b0ee6b38a1aca5a5ddfa2eb6e55
BlackAngelTVdev <dami.scoot3@gmail.com>
2026-01-26 18:26:14 +0100

    Config(1)

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,8 @@ export default defineConfig({
       reload: ['resources/views/**/*.edge'],
     }),
   ],
+
   server: {
-    // Indispensable pour Docker : écoute sur toutes les interfaces
-    host: '0.0.0.0', 
-    // Autorise ton domaine
     allowedHosts: ['dev-sol.laxacube.ch'],
-    // Pour que le rafraîchissement automatique (HMR) fonctionne via ton URL
-    hmr: {
-      host: 'dev-sol.laxacube.ch',
-    },
   },
 })
