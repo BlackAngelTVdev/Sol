b612cbd3d7def3559759ef45d00f6d4c53d01c57
BlackAngelTVdev <dami.scoot3@gmail.com>
2026-02-07 17:39:32 +0100

    Feat(404 and placlder): mets des placolder
    [15][DONE]
    MEts des placolder images sur la home pour le carousel et aussi 404
    custom avec un crayon qui casse ca mine

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,17 @@
 import router from '@adonisjs/core/services/router'
 
-// Ta page "Coming Soon" (Timer)
+// Ta page "Coming Soon"
 router.on('/').render('pages/create')
 
-// Ta nouvelle page d'accueil (Portfolio/Carousel)
+// Ta nouvelle page d'accueil
 router.on('/home').render('pages/home')
+
+// Groupement des routes Portfolio
+router.group(() => {
+  router.on('/original').render('pages/portfolio/original')
+  router.on('/fanart').render('pages/portfolio/fanart')
+  router.on('/commandes').render('pages/portfolio/commandes')
+  router.on('/croquis').render('pages/portfolio/croquis')
+}).prefix('/portfolio')
+
+router.on('404').render('pages/errors/not_found')
